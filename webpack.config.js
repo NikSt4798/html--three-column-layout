be43d1158ad8ba60aa35cc2e9894ac3cb6a9226a
Nikita Stolbovskih <stolbovskikh@speechpro.com>
2021-03-29 01:26:31 +0300

    fix lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+import MiniCssExtractPlugin, {
+  loader as _loader,
+} from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { resolve, join } from "path";
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "./dist"),
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "./dist"),
-    compress: true,
-    port: 9000,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-    new MiniCssExtractPlugin(),
+export const entry = "./src/index.js";
+export const output = {
+  filename: "index.js",
+  path: resolve(__dirname, "./dist"),
+};
+export const devServer = {
+  contentBase: join(__dirname, "./dist"),
+  compress: true,
+  port: 9000,
+};
+export const plugins = [
+  new HtmlWebpackPlugin({
+    template: "src/index.html",
+  }),
+  new MiniCssExtractPlugin(),
+];
+export const module = {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: [_loader, "css-loader"],
+    },
+    {
+      test: /\.html$/i,
+      loader: "html-loader",
+    },
   ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-    ],
-  },
 };
